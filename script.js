a84f37d4df6d153039c41363dd06d5a1bd80f675
[Adoo Jonathan] <[adoojonathan412@gmail.com]>
2023-08-31 19:06:11 +0000

    updates

---
+++ script.js
@@ -0,0 +1,22 @@
+const chatHistory = document.getElementById('chat-history');
+const userInput = document.getElementById('user-input');
+const sendBtn = document.getElementById('send-btn');
+
+sendBtn.addEventListener('click', () => {
+    const message = userInput.ariaValueText;
+    displayMessage('You', message);
+
+    //API call and AI response handling
+    //...
+
+    userInput.value = '';
+});
+
+function displayMessage(sender, message) {
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('message');
+    messageDiv.textContent = `${sender}: ${message}`;
+    chatHistory.appendChild(messageDiv);
+
+    chatHistory.scrollTop = chatHistory.scrollHeight;
+}
